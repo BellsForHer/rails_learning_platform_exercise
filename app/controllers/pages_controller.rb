f32ae3740d8a8047c067f018011737910d6bb5f6
BellsForHer <arhall88@gmail.com>
2023-01-30 20:25:13 -0600

    Create pages_controller.rb

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,27 @@
+class PagesController < ApplicationController
+    def home
+
+    end
+    def about
+
+    end
+    def contact
+
+    end
+    def faq
+
+    end
+    def terms
+
+    end
+    def privacy
+
+    end
+    def course
+
+    end
+    def testimonials
+
+    end
+    
+end
